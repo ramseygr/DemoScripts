d41f30191effc110bd8bcfd6b1ca8c01973b1b46
ramseygr <ramseyg@gmail.com>
2015-11-16 16:01:54 -0800

    Added Tidy Cache Script

---
+++ Release/ConfigMgr/TidyCache/TidyCache_Remediate.ps1
@@ -0,0 +1,7 @@
+﻿#remediate
+$MinDays = 60
+$UIResourceMgr = New-Object -ComObject UIResource.UIResourceMgr
+$Cache = $UIResourceMgr.GetCacheInfo()
+$Cache.GetCacheElements()  | where-object {[datetime]$_.LastReferenceTime -lt (get-date).adddays(-$mindays)} | % {
+$Cache.DeleteCacheElement($_.CacheElementID)
+}
