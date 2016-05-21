c4f32b6027d99af879ce50dc96504ccf2e34cb48
ramseygr <ramseyg@gmail.com>
2016-05-20 20:56:39 -0700

    updated comments

--- Release/ConfigMgr/PendingReboot/PendingReboot_Discovery.ps1
+++ Release/ConfigMgr/PendingReboot/PendingReboot_Discovery.ps1
@@ -1,5 +1,6 @@
 ﻿#Discovery script to report if ConfigMgr says a reboot is pending.
 #http://blog.coretech.dk/kea/dealing-with-reboot-pending-clients-in-configuration-manager-2012/
+#should return false if there is no pending reboot.
 $val = Invoke-WmiMethod -Namespace "ROOT\ccm\ClientSDK" -Class CCM_ClientUtilities -Name DetermineIfRebootPending  | select-object -ExpandProperty "RebootPending"
 $val
 
