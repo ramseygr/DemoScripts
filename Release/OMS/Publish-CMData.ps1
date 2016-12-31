fca64276ee82b8f393a0875921b01359e77797e6
Greg Ramsey <ramseyg@gmail.com>
2016-12-31 08:51:27 -0800

    Update Publish-CMData.ps1

--- Release/OMS/Publish-CMData.ps1
+++ Release/OMS/Publish-CMData.ps1
@@ -12,7 +12,7 @@ $LogType = "CM_CollevalInfo"
 # Specify a field with the created time for the records
 $TimeStampField = "LastEvaluationCompletionTime"
 
-$cnstring = 'Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=CM_AM1;Data Source=auspwcfgam1db;User ID=;Password='
+$cnstring = 'Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=CM_PS1;Data Source=mySiteServerDB;User ID=;Password='
 
 
 $cmdtext = @"
