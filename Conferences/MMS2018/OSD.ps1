01dd6603a2911f69566f60ed0646006bdfc60bec
ramseygr <ramseyg@gmail.com>
2018-05-17 17:18:29 -0500

    mms 2018 scripts
    mms 2018 scripts

---
+++ Conferences/MMS2018/OSD.ps1
@@ -0,0 +1,109 @@
+﻿Exit
+#Warning - CM TS cmdlets are **really powerful**. 
+#    Make sure to test them out in lab before using them in Production!
+
+
+
+
+#replace old 7-zip package/program with new one, and add wmi condition
+
+
+$OldPackage = Get-CMPackage -Name '7-zip' | 
+    Where-Object {$_.version -eq '16.04'}
+
+$OldProgram = Get-CMProgram -Package $OldPackage `
+    -ProgramName 'Install x64'
+
+$NewPackage = Get-CMPackage -Name '7-zip' | 
+    Where-Object {$_.version -eq '18.05'}
+
+$NewProgram = Get-CMProgram -Package $NewPackage `
+    -ProgramName 'Install x64'
+
+
+#Get the TS
+$CMTaskSequence = Get-CMTaskSequence -Name 'IT Software Stack'
+
+$NewPrgCondition = New-CMTaskSequenceStepConditionQueryWMI `
+    -Query "select * from Win32_OperatingSystem where OSArchitecture ='64-bit'"
+
+$NewPackageTSStep = New-CMTaskSequenceStepInstallSoftware `
+    -Program $NewProgram -Name 'Install 7-Zip 18.05' `
+    -Condition $NewPrgCondition
+
+
+#Add the TS Step
+Set-CMTaskSequenceGroup -AddStep $NewPackageTSStep `
+    -InputObject $CMTaskSequence -StepName 'Standard Software'
+
+#Remove the old TS Step
+Remove-CMTaskSequenceStep -InputObject $CMTaskSequence `
+    -StepName 'Install 7-Zip 16.04'
+
+
+
+
+
+
+
+
+
+#region Lab Prep before the session
+#New sample driver package for OSD
+    New-CMPackage -Name 'OSD DRV - Dell Latitude 7280'
+#endregion
+
+#Add two new Task Sequence Groups
+#Get the TS first
+New-CMTaskSequence -Name 'Drivers' -CustomTaskSequence
+
+$CMTaskSequence = Get-CMTaskSequence -Name 'Drivers'
+#Define new TS Groups
+$Microsoft = New-CMTaskSequenceGroup -Name 'Microsoft'
+$Lenovo = New-CMTaskSequenceGroup -Name 'Lenovo'
+$HP = New-CMTaskSequenceGroup -Name 'HP'
+$Dell = New-CMTaskSequenceGroup -Name 'Dell'
+$BIOSUpdates = New-CMTaskSequenceGroup -Name 'BIOS Updates'
+
+$Drivers = New-CMTaskSequenceGroup -Name 'Drivers' `
+    -Step @($Microsoft,$Lenovo,$HP, $Dell)
+
+
+#Add the groups first. Index == order nr in TS
+Add-CMTaskSequenceStep -Step $BIOSUpdates `
+    -InputObject $CMTaskSequence -InsertStepStartIndex 1
+
+Add-CMTaskSequenceStep -Step $Drivers -InputObject $CMTaskSequence `
+    -InsertStepStartIndex 2
+
+
+#Get the "DRV" Package
+$NewCMDriverPackage = Get-CMPackage -Name 'OSD DRV - Dell Latitude 7280'
+#Get the TS
+$CMTaskSequence = Get-CMTaskSequence -Name 'Drivers'
+
+#New DRV commandline to inject the drivers during OSD
+$DRVCommandLine = 'DISM.exe /Image:%OSDisk%\ /Add-Driver /Driver:.\ /Recurse'
+
+#Get the Drivers/Dell Group
+$Drivers = Get-CMTaskSequenceGroup -InputObject $CMTaskSequence -StepName Dell
+
+#New TS DRV Condition - WMI
+$NewDRVCondition = New-CMTaskSequenceStepConditionQueryWMI `
+    -Query "Select * from Win32_ComputerSystem where model like '%Latitude 7280%'"
+
+#Define TS Command-line Step
+$NewTSCommandLine = New-CMTaskSequenceStepRunCommandLine `
+    -CommandLine $DRVCommandLine `
+    -PackageId $NewCMDriverPackage.PackageID `
+    -SuccessCode @(0,3010,2,50) `
+    -Name $NewCMDriverPackage.Name -Condition $NewDRVCondition
+
+#Add the TS Step
+Set-CMTaskSequenceGroup -AddStep $NewTSCommandLine `
+    -InputObject $CMTaskSequence -StepName Microsoft
+
+
+
+
+
---
+++ Conferences/MMS2018/scripts.ps1
@@ -0,0 +1,117 @@
+﻿#scripts
+****
+Exit
+
+
+#Show how to get info on Service - "Get BITS Service Status"
+#add some (very basic) Error Checking.... - "Get BITS Service Status With Error Checking"
+
+try {
+    Get-CimInstance win32_service -filter "Name='BITS'" |
+         select Status -ExpandProperty Status
+}
+catch {
+    "Error"
+    Exit -1
+}
+
+
+
+
+#structured output - with parameters -example with 
+#process name -wmiprvse and w3wp
+Param([Parameter(Mandatory=$true)][string]$ProcessName)
+try {
+    Get-Process -Name $ProcessName | 
+        select ProcessName, Handles, WS, NPM, PM, CPU 
+}
+catch {
+    "Error"
+    exit -1
+}
+
+
+
+
+
+
+
+#Get under the hood
+
+Get-CimInstance -Namespace root\sms\site_tp1 `
+     -ClassName SMS_Scripts | Out-GridView
+
+
+Invoke-CMWmiQuery "select * from sms_scripts" | out-gridview
+
+Invoke-CMWmiQuery "Select * from SMS_ScriptsExecutionStatus" | Out-GridView
+
+
+
+#get the status for the last script to run
+$wql = @"
+    Select distinct ClientOperationID 
+    from SMS_ScriptsExecutionStatus 
+    order by ClientOperationID desc
+"@
+$LastRun = (Invoke-CMWmiQuery $wql ).ClientOperationID[0]
+
+#query for last one based on ClientOperationID
+$wql = @"
+    Select * from SMS_ScriptsExecutionStatus
+    where ClientOperationID=$lastrun
+"@
+$LastRunDetails = Invoke-CMWmiQuery $wql
+$LastRunDetails | Out-GridView
+
+
+#get the data to where we can use it...
+$collobj = @()
+$LastRunDetails | foreach {
+    $Device = $_.DeviceName
+    $_.ScriptOutput | ConvertFrom-Json | foreach {
+        $_ | Add-Member –MemberType NoteProperty –Name DeviceName –Value $Device
+        $collobj += $_
+    }
+}
+
+$collobj | Out-GridView
+
+
+
+#Re-run TS - TP120002 - this assumes the TS is already deployed to the system,
+# with a future mandatory date
+Param([Parameter(Mandatory=$true)][string]$DeploymentID)
+$strFilter = "ScheduledMessageID like '$DeploymentID%'"
+
+$deployment = gwmi ccm_scheduler_scheduledmessage `
+    -Namespace root\ccm\policy\machine\actualconfig -filter $strFilter 
+if (($deployment | measure).count -eq 0) {
+     "Deployment Not Found!"
+     exit -1
+     } 
+     else {
+    $retval =Invoke-WmiMethod -Namespace root\ccm -Class sms_client `
+        -Name TriggerSchedule $deployment.ScheduledMessageID
+}
+
+
+
+
+#Approve a script pending approval
+#Approve Scripts ApprovalState: 3=approved 0=pending
+
+#pending approvals
+$Pending = Get-WmiObject -class sms_scripts -namespace root\sms\site_TP1 `
+    -filter "ApprovalState=0"
+$Pending | Out-GridView
+
+$env:USERNAME
+$Pending.UpdateApprovalState("3", "gmr\ramseygrgoat", "approved")
+
+
+
+
+#??What just happened?
+
+
