68a7f99cf34bdb761889eaac2e9869abefacb84f
Greg Ramsey <ramseyg@gmail.com>
2018-12-01 14:57:08 -0600

    New foldedr for MMS DE 2018

---
+++ Conferences/MMSDE2018/Start.ps1
@@ -0,0 +1,10 @@
+﻿
+#region Import CM Module
+$CMModulePath = `
+    $Env:SMS_ADMIN_UI_PATH.ToString().SubString(0,$Env:SMS_ADMIN_UI_PATH.Length - 5) `
+    + "\ConfigurationManager.psd1" 
+Import-Module $CMModulePath -force
+cd TP1:
+#endregion
+
+
