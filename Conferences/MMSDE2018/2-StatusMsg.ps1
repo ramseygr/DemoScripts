a4ae7a7d96d0fcefd62401c264b7c3ec278906b7
Greg Ramsey <ramseyg@gmail.com>
2018-12-03 10:29:21 -0600

    updates

---
+++ Conferences/MMSDE2018/2-StatusMsg.ps1
@@ -0,0 +1,44 @@
+﻿#https://gregramsey.net/2014/05/19/running-actions-based-on-configmgr-status-filter-rules-with-powershell/
+
+#C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command "& {C:\Scripts\StatusFilter\NoSecretDeployments.ps1 -computername '%msgsys' -msgdesc '%msgdesc' -UserName '%msgis01' -ApplicationName '%msgis03' -CollectionName '%msgis04' }"
+#status filter variables: http://technet.microsoft.com/en-us/library/bb693758.aspx
+#New Deployment of Application
+#Source: SMS Provider
+#Message ID: 30226
+
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True)]
+   [string]$ComputerName,
+	
+  [Parameter(Mandatory=$True)]
+   [string]$MSGDesc,
+
+  [Parameter(Mandatory=$True)]
+   [string]$UserName,
+
+  [Parameter(Mandatory=$True)]
+   [string]$ApplicationName,
+
+  [Parameter(Mandatory=$True)]
+   [string]$CollectionName,
+   	
+  [Parameter()]
+   [string]$AdditionalInfo
+)
+
+function write-log ($string)
+{
+    $string + " " + (get-date) | Out-File C:\Scripts\StatusFilter\NoSecrets\NoSecretRequiredDeployments.log -Append
+}
+
+write-log ("Start ")
+write-log ("Source " + $ComputerName)
+write-log ($MSGDesc)
+
+write-log ("Importing ConfigMgr Module")
+Import-Module $env:SMS_ADMIN_UI_PATH.Replace("\bin\i386","\bin\configurationmanager.psd1")$SiteCode = Get-PSDrive -PSProvider CMSITESet-Location "$($SiteCode.Name):\"
+
+write-log ("Querying Application Name")
+write-log ($ApplicationName)
+write-log ($CollectionName)
--- Conferences/MMSDE2018/Start.ps1
+++ Conferences/MMSDE2018/Start.ps1
@@ -1,4 +1,10 @@
 ﻿#Demo 1
-psedit "C:\Users\ramseygr\Documents\GitHub\DemoScripts\Conferences\MMSDE2018\1.ps1"
+psedit "C:\Users\ramseygr\Documents\GitHub\DemoScripts\Conferences\MMSDE2018\1-overview.ps1"
+psedit "C:\Users\ramseygr\Documents\GitHub\DemoScripts\Conferences\MMSDE2018\2-statusmsg.ps1"
 
 #Demo 2
+
+
+
+#PowerShell for Beginners:
+#https://github.com/ramseygr/DemoScripts/tree/master/Conferences/DevCon2017/PowerShellForBeginners
